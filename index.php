292b9e121f0187dd1f724e752994dbcb8f650835
DiegoFarrugio <diegofarrugio0@gmail.com>
2024-02-06 15:29:42 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- FONT-FAMILY -->
+
+        <!-- LINK CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/style.css">
+        <!-- TITLE PAGE -->
+        <title>Document</title>
+    </head>
+
+        <!-- START BODY -->
+    <body>
+        <!-- START MAIN HEADER -->
+        <header>
+            <div>
+                HEAD
+            </div>
+        </header>
+        <!-- END MAIN HEADER -->
+
+        <!-- START MAIN -->
+        <main>
+            <div>
+                MAIN
+            </div>
+        </main>
+        <!-- END MAIN -->
+
+        <!-- START FOOTER -->
+        <footer>
+            <div>
+                FOOTER
+            </div>
+        </footer>
+        <!-- END FOOTER -->
+    </body>
+</html>
